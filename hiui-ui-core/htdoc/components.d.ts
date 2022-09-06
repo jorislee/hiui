8d72d2f5ee031dd06d45d49391e55347a808e153
zzzzzhy <11657924@qq.com>
2022-09-06 11:06:47 +0800

    create:wireless组件

--- hiui-ui-core/htdoc/components.d.ts
+++ hiui-ui-core/htdoc/components.d.ts
@@ -13,6 +13,7 @@ declare module '@vue/runtime-core' {
     NCard: typeof import('naive-ui')['NCard']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
+    NDataTable: typeof import('naive-ui')['NDataTable']
     NDescriptions: typeof import('naive-ui')['NDescriptions']
     NDescriptionsItem: typeof import('naive-ui')['NDescriptionsItem']
     NDialogProvider: typeof import('naive-ui')['NDialogProvider']
