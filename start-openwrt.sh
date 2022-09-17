90fe58cd53d7a4ab97c1983be542c7bf491d86c4
zzzzzhy <11657924@qq.com>
2022-09-17 23:09:01 +0800

    update

---
+++ start-openwrt.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+qemu-system-aarch64 -m 1024 -smp 2 -cpu host -M virt,highmem=off \
+    -nographic \
+    -accel hvf \
+    -kernel /Users/ruby/Downloads/openwrt-22.03.0-armvirt-64-Image \
+    -drive file=/Users/ruby/Downloads/openwrt-22.03.0-armvirt-64-rootfs-squashfs.img,format=raw,if=virtio \
+    -append root=/dev/vda \
+    -device virtio-net,netdev=net0 -netdev user,id=net0,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:1122-192.168.1.254:22 \
+    -device virtio-net,netdev=net1 -netdev user,id=net1,net=192.0.2.0/24
