5f47b80af94b731e625721405aa3fbfc09f12bf2
zzzzzhy <11657924@qq.com>
2022-09-18 04:07:28 +0800

    add nginx

--- start-openwrt.sh
+++ start-openwrt.sh
@@ -6,5 +6,5 @@ qemu-system-aarch64 -m 1024 -smp 2 -cpu host -M virt,highmem=off \
     -kernel /Users/ruby/Downloads/openwrt-22.03.0-armvirt-64-Image \
     -drive file=/Users/ruby/Downloads/openwrt-22.03.0-armvirt-64-rootfs-squashfs.img,format=raw,if=virtio \
     -append root=/dev/vda \
-    -device virtio-net,netdev=net0 -netdev user,id=net0,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:1122-192.168.1.254:22 \
+    -device virtio-net,netdev=net0 -netdev user,id=net0,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:1122-192.168.1.254:22,hostfwd=tcp:127.0.0.1:2333-192.168.1.254:80 \
     -device virtio-net,netdev=net1 -netdev user,id=net1,net=192.0.2.0/24
